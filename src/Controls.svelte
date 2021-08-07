f61a16ada1d6039e93f80b1bc4b67e291f5333f5
Mattias Olla <mattias.olla@gmail.com>
2021-08-07 11:45:49 +0200

    Stop running when  "Clear" is pressed

--- src/Controls.svelte
+++ src/Controls.svelte
@@ -8,12 +8,17 @@
   const toggleRunning = () => {
     running = !running;
   };
+
+  const onClear = () => {
+    running = false;
+    dispatch('clear');
+  };
 </script>
 
 <main>
   <button on:click={() => dispatch('step')}>Step</button>
   <button on:click={toggleRunning}>{running ? 'Stop' : 'Run'}</button>
-  <button on:click={() => dispatch('clear')}>Clear</button>
+  <button on:click={onClear}>Clear</button>
 </main>
 
 <style>
