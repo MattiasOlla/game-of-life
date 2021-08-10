5a2c5d6e7c598ac4f5856de6aa576e003b349076
Mattias Olla <mattias.olla@gmail.com>
2021-08-10 18:09:08 +0200

    Add input elements to save the current config

--- src/SaveConfigs.svelte
+++ src/SaveConfigs.svelte
@@ -8,6 +8,9 @@
   let selectedConfig: Config;
   let storedConfigs = configs;
 
+  let showSaveInput = false;
+  let saveInputName = '';
+
   onMount(() => {
     const savedConfig = localStorage.getItem('configs');
     if (savedConfig) storedConfigs = JSON.parse(savedConfig);
@@ -15,12 +18,11 @@
 
   // Save to localstorage every time the storedConfigs is updated
   $: localStorage.setItem('configs', JSON.stringify(storedConfigs));
-  $: console.log(storedConfigs);
 </script>
 
 <main>
   <label for="stored_configs">Select a configuration:</label>
-  <div>
+  <div class="row">
     <select id="stored_configs" bind:value={selectedConfig}>
       {#each storedConfigs as config, i (i)}
         <option value={config}>{config.name}</option>
@@ -28,7 +30,20 @@
     </select>
     <button on:click={() => dispatch('change', selectedConfig)}>Load config</button>
   </div>
-  <button on:click>Save current configuration</button>
+
+  <button
+    on:click={() => {
+      showSaveInput = true;
+      saveInputName = '';
+    }}>Save current configuration</button
+  >
+  <div class="card" class:hidden={!showSaveInput}>
+    <input type="text" placeholder="Enter configuration name" bind:value={saveInputName} />
+    <div class="row">
+      <button>Save</button>
+      <button on:click={() => (showSaveInput = false)}>Cancel</button>
+    </div>
+  </div>
 </main>
 
 <style>
@@ -37,8 +52,25 @@
     flex-direction: column;
   }
 
-  main > div {
+  .hidden {
+    display: none !important;
+  }
+
+  .row {
+    display: flex;
     flex-direction: row;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: 0.8rem;
+    overflow: hidden;
+  }
+
+  .card > input {
+    width: 90%;
   }
 
   label {
